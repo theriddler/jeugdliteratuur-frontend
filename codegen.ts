e41f4a7b9facba1ee795311dd569de06908c8e16
theriddler <riddle.hunter@gmail.com>
2025-09-29 12:55:09 +1300

    codegen needs to point to local server

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
-  schema: 'https://renowned-chocolate-b2365884fe.strapiapp.com/graphql/',
+  schema: 'http://localhost:1337/graphql/',
   documents: [ 'src/**/*.ts', 'src/**/*.tsx' ],
   generates: {
     './src/gql/': {
