3af9520e21994b1dc8cafe35191364b47c32dee3
theriddler <riddle.hunter@gmail.com>
2025-09-29 13:31:37 +1300

    bring introduction into homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,7 +1,14 @@
+import { useQuery } from "@apollo/client"
+import { INTRODUCTION } from "../queries"
+
 export const Homepage = () => {
+  const { data } = useQuery(INTRODUCTION)
+  console.log(data)
+
   return (
     <div>
-      <h5> This it the Homepage</h5>
+      <h5>Homepage</h5>
+      {data?.introduction?.Text}
     </div>
   )
 }
