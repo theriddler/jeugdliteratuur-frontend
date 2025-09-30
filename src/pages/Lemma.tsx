5f076459a54b2d17bcdb9007fd56a336f33569a9
theriddler <riddle.hunter@gmail.com>
2025-09-30 21:56:16 +1300

    prettier and stuff works

--- src/pages/Lemma.tsx
+++ src/pages/Lemma.tsx
@@ -22,17 +22,22 @@ export const Lemma = () => {
       )}
       <Row>
         <Col xs={12}>
-          <div>
-            <h4>{data?.lemma?.titel}</h4>
-          </div>
-          {data?.lemma?.beschrijving && (
-            <div>
+          <div className="lemma-header">
+            <div className="d-flex gap-3 align-items-end">
+              <div>
+                <h4>{data?.lemma?.titel} ({data?.lemma?.jaar})</h4>
+              </div>
+              <div className="text-secondary">
+                {data?.lemma?.auteurVoornaam} {data?.lemma?.auterAchternaam}
+              </div>
+            </div>
+            <div className="mt-3">
               {data?.lemma?.beschrijving}
             </div>
-          )}
+          </div>
         </Col>
         <Col xs={12}>
-          <div>
+          <div className="lemma-section">
             <h5>Motieven</h5>
             {data?.lemma?.motieven && (
               <BlocksRenderer content={data?.lemma?.motieven} />
@@ -40,7 +45,7 @@ export const Lemma = () => {
           </div>
         </Col>
         <Col xs={12}>
-          <div>
+          <div className="lemma-section">
             <h5>Doelgroep</h5>
             {data?.lemma?.doelgroep && (
               <BlocksRenderer content={data?.lemma?.doelgroep} />
@@ -48,15 +53,7 @@ export const Lemma = () => {
           </div>
         </Col>
         <Col xs={12}>
-          <div>
-            <h5>Doelgroep</h5>
-            {data?.lemma?.doelgroep && (
-              <BlocksRenderer content={data?.lemma?.doelgroep} />
-            )}
-          </div>
-        </Col>
-        <Col xs={12}>
-          <div>
+          <div className="lemma-section">
             <h5>Het verhaal</h5>
             {data?.lemma?.hetVerhaal && (
               <BlocksRenderer content={data?.lemma?.hetVerhaal} />
@@ -64,7 +61,7 @@ export const Lemma = () => {
           </div>
         </Col>
         <Col xs={12}>
-          <div>
+          <div className="lemma-section">
             <h5>Analyse en interpretatie</h5>
             {data?.lemma?.analyse && (
               <BlocksRenderer content={data?.lemma?.analyse} />
@@ -72,7 +69,7 @@ export const Lemma = () => {
           </div>
         </Col>
         <Col xs={12}>
-          <div>
+          <div className="lemma-section">
             <h5>Didactische vragen en lessuggesties</h5>
             {data?.lemma?.lessuggesties && (
               <BlocksRenderer content={data?.lemma?.lessuggesties} />
@@ -80,7 +77,7 @@ export const Lemma = () => {
           </div>
         </Col>
         <Col xs={12}>
-          <div>
+          <div className="lemma-section">
             <h5>Kerndoelen Nederlands</h5>
             {data?.lemma?.kerndoelen && (
               <BlocksRenderer content={data?.lemma?.kerndoelen} />
@@ -88,22 +85,22 @@ export const Lemma = () => {
           </div>
         </Col>
         <Col xs={12}>
-          <div>
+          <div className="lemma-section">
             <h5>Thematische tags</h5>
           </div>
         </Col>
         <Col xs={12}>
-          <div>
+          <div className="lemma-section">
             <h5>⁠Opstaptitels</h5>
           </div>
         </Col>
         <Col xs={12}>
-          <div>
+          <div className="lemma-section">
             <h5>Verder lezen</h5>
           </div>
         </Col>
         <Col xs={12}>
-          <div>
+          <div className="lemma-section">
             <h5>Vindplaatsen en bronnen</h5>
           </div>
         </Col>
