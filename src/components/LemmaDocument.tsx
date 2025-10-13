07690e0ecd19389f4b68c85eecbf64231e5fd11f
theriddler <riddle.hunter@gmail.com>
2025-10-13 14:29:00 +1300

    downloading now works properly

---
+++ src/components/LemmaDocument.tsx
@@ -0,0 +1,205 @@
+import { Document, Page, PDFDownloadLink, View } from "@react-pdf/renderer";
+import { BlocksRenderer } from "@strapi/blocks-react-renderer";
+import ReactDOMServer from 'react-dom/server';
+import Html from "react-pdf-html";
+import { Col, Row } from "reactstrap";
+import { LemmaEntity } from "../gql/graphql";
+
+export const LemmaDocumentReact = (props: {
+  lemma: LemmaEntity | undefined
+}) => {
+  if (!props.lemma) return null;
+  const { attributes } = props.lemma;
+
+  return (
+    <div>
+      <Row>
+        <Col xs={12} lg={8}>
+          <div className="lemma-header">
+            <div>
+              <div className="d-flex justify-content-between align-items-end">
+                <div className="d-flex gap-3 align-items-end">
+                  <div>
+                    <h3>{attributes?.titel} ({attributes?.jaar})</h3>
+                  </div>
+                  <div className="text-secondary">
+                    {attributes?.auteur_voornaam} {attributes?.auter_achternaam}
+                  </div>
+                </div>
+                <div>
+                  <PDFDownloadLink className="hide-in-pdf download-pdf-link" document={<LemmaDocument lemma={props.lemma as LemmaEntity} />}>
+                    Download PDF
+                  </PDFDownloadLink>
+                </div>
+              </div>
+              <div className="mt-3">
+                {attributes?.de_kern && (
+                  <BlocksRenderer content={attributes?.de_kern} />
+                )}
+              </div>
+            </div>
+          </div>
+        </Col>
+        <Col xs={12} lg={4}>
+          <div className="h-100 d-flex align-items-center justify-content-end">
+            <div className="image-wrapper lemma-header">
+              <img src={attributes?.afbeelding?.data?.attributes?.url} />
+            </div>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} lg={8}>
+          {attributes?.doelgroep && (
+            <section>
+              <div className="lemma-main-section">
+                <h5>Doelgroep</h5>
+                <BlocksRenderer content={attributes?.doelgroep} />
+              </div>
+            </section>
+          )}
+          {attributes?.motieven && (
+            <section>
+              <div className="lemma-main-section">
+                <h5>Motieven</h5>
+                <BlocksRenderer content={attributes?.motieven} />
+              </div>
+            </section>
+          )}
+          {attributes?.het_verhaal && (
+            <section>
+              <div className="lemma-main-section">
+                <h5>Het verhaal</h5>
+                <BlocksRenderer content={attributes?.het_verhaal} />
+              </div>
+            </section>
+          )}
+          {attributes?.analyse && (
+            <section>
+              <div className="lemma-main-section">
+                <h5>Analyse en interpretatie</h5>
+                <BlocksRenderer content={attributes?.analyse} />
+              </div>
+            </section>
+          )}
+          {attributes?.lessuggesties && (
+            <section>
+              <div className="lemma-main-section">
+                <h5>Didactische vragen en lessuggesties</h5>
+                <BlocksRenderer content={attributes?.lessuggesties} />
+              </div>
+            </section>
+          )}
+          {attributes?.kerndoelen && (
+            <section>
+              <div className="lemma-main-section">
+                <h5>Kerndoelen Nederlands</h5>
+                <BlocksRenderer content={attributes?.kerndoelen} />
+              </div>
+            </section>
+          )}
+          <section>
+            <div className="lemma-main-section">
+              <h5>Vindplaatsen en bronnen</h5>
+              {attributes?.bronnen && (
+                <BlocksRenderer content={attributes?.bronnen} />
+              )}
+            </div>
+          </section>
+        </Col>
+        <Col xs={12} lg={4}>
+          {/* <div className="lemma-side-section">
+            <h5>Thematische tags</h5>
+          </div> */}
+          <div className="lemma-side-section">
+            <h5>⁠Opstaptitels</h5>
+            {attributes?.opstaptitels_extern && (
+              <BlocksRenderer content={attributes?.opstaptitels_extern} />
+            )}
+          </div>
+          <div className="lemma-side-section">
+            <h5>Verder lezen</h5>
+            {attributes?.verder_lezen_extern && (
+              <BlocksRenderer content={attributes?.verder_lezen_extern} />
+            )}
+          </div>
+        </Col>
+      </Row >
+    </div >
+  )
+}
+
+
+// Create styles
+const stylesheet = {
+  body: {
+    fontSize: 12,
+    paddingLeft: 8,
+    paddingRight: 8,
+    paddingTop: 2,
+    paddingBottom: 2,
+  },
+  p: {
+    fontSize: 12,
+    marginBottom: 0
+  },
+  h3: {
+    fontSize: 18,
+    marginBottom: 2,
+    marginTop: 1
+  },
+  h5: {
+    fontSize: 16,
+    marginBottom: 0
+  },
+  ul: {
+    width: '100%',
+    marginBottom: 0
+  },
+  [ '.lemma-main-section' ]: {
+    padding: 2
+  },
+  [ '.hide-in-pdf' ]: {
+    color: 'transparent'
+  }
+};
+
+
+const UnbreakableView = ({ children }: { children: React.ReactNode }) => (
+  <View wrap={false}>
+    {children}
+  </View>
+);
+
+export const LemmaDocument = (props: {
+  lemma: LemmaEntity | undefined
+}) => {
+  if (!props.lemma) return null;
+  const html = ReactDOMServer.renderToStaticMarkup(<LemmaHTML lemma={props.lemma} />)
+
+  return (
+    <Document>
+      <Page size='A4'>
+        <Html
+          stylesheet={stylesheet}
+          renderers={{
+            // Map the class name to your custom component
+            'section': UnbreakableView
+          }}
+        >
+          {html}
+        </Html>
+      </Page>
+    </Document>
+  )
+}
+
+const LemmaHTML = (props: {
+  lemma: LemmaEntity
+}) => (
+  <html>
+    <body>
+      <LemmaDocumentReact lemma={props.lemma} />
+    </body>
+  </html>
+)
--- src/pages/Lemma.tsx
+++ src/pages/Lemma.tsx
@@ -1,11 +1,9 @@
 import { useQuery } from "@apollo/client";
-import { BlocksRenderer } from "@strapi/blocks-react-renderer";
-import { IconFileText } from "@tabler/icons-react";
-import html2canvas from "html2canvas";
-import jsPDF from "jspdf";
-import { useMemo, useRef } from "react";
+import { useMemo } from "react";
 import { useParams } from "react-router";
-import { Button, Col, Row, Spinner } from "reactstrap";
+import { Spinner } from "reactstrap";
+import { LemmaDocumentReact } from "../components/LemmaDocument";
+import { LemmaEntity } from "../gql/graphql";
 import { LEMMATA } from "../queries";
 
 export const Lemma = () => {
@@ -14,169 +12,14 @@ export const Lemma = () => {
   const { data, loading } = useQuery(LEMMATA);
   const lemma = useMemo(() => data?.lemmata?.data?.find(l => l.id === lemmaId), [ data?.lemmata?.data, lemmaId ])
 
-  // download PDF logic
-  const pdfElementRef = useRef<HTMLDivElement | null>(null);
-  const downloadPDF = async () => {
-    const element = pdfElementRef.current;
-    if (element) {
-      element.classList.add('print-mode')
-
-      const canvas = await html2canvas(element, {
-        scale: 2, // Higher scale often results in crisper text/images
-        useCORS: true // Essential if you have images from different domains
-      });
-
-      // Get the image data from the canvas
-      const imgData = canvas.toDataURL('image/png');
-      const imgWidth = canvas.width;
-      const imgHeight = canvas.height;
-
-      // Setup the PDF
-      // 'p' for portrait, 'mm' for millimeters, 'a4' for page size
-      const pdf = new jsPDF('p', 'mm', 'a4');
-      const pdfWidth = pdf.internal.pageSize.getWidth();
-      const pdfHeight = pdf.internal.pageSize.getHeight();
-
-      // Calculate the aspect ratio to fit the image to the PDF page width
-      const ratio = imgWidth / pdfWidth;
-      const canvasHeightInPDF = imgHeight / ratio;
-
-      let heightLeft = canvasHeightInPDF;
-      let position = 0; // Y-position on the PDF page
-
-      // Add the first page
-      pdf.addImage(imgData, 'PNG', 0, position, pdfWidth, canvasHeightInPDF);
-      heightLeft -= pdfHeight;
-
-      // Add new pages as long as there's content left
-      while (heightLeft > 0) {
-        position = position - pdfHeight; // Move the image "up" on the next page
-        pdf.addPage();
-        pdf.addImage(imgData, 'PNG', 0, position, pdfWidth, canvasHeightInPDF);
-        heightLeft -= pdfHeight;
-      }
-
-      // Save the PDF
-      pdf.save('download.pdf');
-
-      element.classList.remove('print-mode')
-    }
-  }
-
-  if (!lemma) return null;
-  const { attributes } = lemma;
-
   return (
     <div>
       {loading && (
-        <Spinner />
+        <div className="w-100 h-100 d-flex align-items-center justify-content-center">
+          <Spinner />
+        </div>
       )}
-      <div ref={pdfElementRef}>
-        <Row>
-          <Col xs={12} lg={8}>
-            <div className="lemma-header">
-              <div>
-                <div className="d-flex justify-content-between align-items-end">
-                  <div className="d-flex gap-3 align-items-end">
-                    <div>
-                      <h3>{attributes?.titel} ({attributes?.jaar})</h3>
-                    </div>
-                    <div className="text-secondary">
-                      {attributes?.auteur_voornaam} {attributes?.auter_achternaam}
-                    </div>
-                  </div>
-                  <Button
-                    outline
-                    size="sm"
-                    className="hide-in-print-mode"
-                    onClick={downloadPDF}
-                  >
-                    <span className="me-2">
-                      <IconFileText />
-                    </span>
-                    <span>Download PDF</span>
-                  </Button>
-                </div>
-                <div className="mt-3">
-                  {attributes?.de_kern && (
-                    <BlocksRenderer content={attributes?.de_kern} />
-                  )}
-                </div>
-              </div>
-            </div>
-          </Col>
-          <Col xs={12} lg={4}>
-            <div className="h-100 d-flex align-items-center justify-content-center">
-              <div className="image-wrapper lemma-header">
-                <img src={attributes?.afbeelding?.data?.attributes?.url} />
-              </div>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12} lg={8}>
-            {attributes?.doelgroep && (
-              <div className="lemma-main-section">
-                <h5>Doelgroep</h5>
-                <BlocksRenderer content={attributes?.doelgroep} />
-              </div>
-            )}
-            {attributes?.motieven && (
-              <div className="lemma-main-section">
-                <h5>Motieven</h5>
-                <BlocksRenderer content={attributes?.motieven} />
-              </div>
-            )}
-            {attributes?.het_verhaal && (
-              <div className="lemma-main-section">
-                <h5>Het verhaal</h5>
-                <BlocksRenderer content={attributes?.het_verhaal} />
-              </div>
-            )}
-            {attributes?.analyse && (
-              <div className="lemma-main-section">
-                <h5>Analyse en interpretatie</h5>
-                <BlocksRenderer content={attributes?.analyse} />
-              </div>
-            )}
-            {attributes?.lessuggesties && (
-              <div className="lemma-main-section">
-                <h5>Didactische vragen en lessuggesties</h5>
-                <BlocksRenderer content={attributes?.lessuggesties} />
-              </div>
-            )}
-            {attributes?.kerndoelen && (
-              <div className="lemma-main-section">
-                <h5>Kerndoelen Nederlands</h5>
-                <BlocksRenderer content={attributes?.kerndoelen} />
-              </div>
-            )}
-            <div className="lemma-main-section">
-              <h5>Vindplaatsen en bronnen</h5>
-              {attributes?.bronnen && (
-                <BlocksRenderer content={attributes?.bronnen} />
-              )}
-            </div>
-          </Col>
-          <Col xs={12} lg={4}>
-            {/* <div className="lemma-side-section">
-            <h5>Thematische tags</h5>
-          </div> */}
-            <div className="lemma-side-section">
-              <h5>⁠Opstaptitels</h5>
-              {attributes?.opstaptitels_extern && (
-                <BlocksRenderer content={attributes?.opstaptitels_extern} />
-              )}
-            </div>
-            <div className="lemma-side-section">
-              <h5>Verder lezen</h5>
-              {attributes?.verder_lezen_extern && (
-                <BlocksRenderer content={attributes?.verder_lezen_extern} />
-              )}
-            </div>
-          </Col>
-        </Row >
-      </div>
+      <LemmaDocumentReact lemma={lemma as LemmaEntity} />
     </div >
   )
 }
