8e63237025268632245c3edf257c92f05fe59ad9
theriddler <riddle.hunter@gmail.com>
2025-10-13 13:11:08 +1300

    level `LemmaOverview` are all same length

--- src/components/LemmaOverview.tsx
+++ src/components/LemmaOverview.tsx
@@ -14,34 +14,38 @@ export const LemmaOverview = (props: {
   const imageUrl = attributes?.afbeelding?.data?.attributes?.url;
 
   return (
-    <Card className="clickable" onClick={() => navigate(`/lemma/${id}`)}>
+    <Card className="clickable h-100" onClick={() => navigate(`/lemma/${id}`)}>
       <CardBody>
-        <div>
+        <div className="d-flex flex-column h-100">
           <div>
-            <span className="fw-bold">{attributes?.titel}</span>
-            <span className="ms-2">({attributes?.jaar})</span>
+            <div>
+              <div>
+                <span className="fw-bold">{attributes?.titel}</span>
+                <span className="ms-2">({attributes?.jaar})</span>
+              </div>
+              <div className="text-secondary">
+                {attributes?.auteur_voornaam} {attributes?.auter_achternaam}
+              </div>
+            </div>
+            <div className="mt-3 d-flex justify-content-center">
+              <div className="image-wrapper lemma-overview">
+                <img src={imageUrl} />
+              </div>
+            </div>
+            <hr />
+            <div className="d-flex justify-content-between">
+              <div>
+                {attributes?.korte_intro && (
+                  <BlocksRenderer content={attributes?.korte_intro} />
+                )}
+              </div>
+            </div>
           </div>
-          <div className="text-secondary">
-            {attributes?.auteur_voornaam} {attributes?.auter_achternaam}
+          <div className="flex-grow-1 d-flex align-items-end">
+            <Link to={`/lemma/${id}`}>
+              Verder lezen
+            </Link>
           </div>
-        </div>
-        <div className="mt-3 d-flex justify-content-center">
-          <div className="image-wrapper">
-            <img src={imageUrl} />
-          </div>
-        </div>
-        <hr />
-        <div className="d-flex justify-content-between">
-          <div>
-            {attributes?.korte_intro && (
-              <BlocksRenderer content={attributes?.korte_intro} />
-            )}
-          </div>
-        </div>
-        <div className="mt-1">
-          <Link to={`/lemma/${id}`}>
-            Verder lezen
-          </Link>
         </div>
       </CardBody>
     </Card>
