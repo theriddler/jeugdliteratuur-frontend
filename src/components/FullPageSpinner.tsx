8b8b631436ad1b1dcc1ce05ea0f503eb9de2cfd5
theriddler <riddle.hunter@gmail.com>
2025-10-17 13:45:30 +1300

    make `FullPageSpinner`

---
+++ src/components/FullPageSpinner.tsx
@@ -0,0 +1,7 @@
+import { Spinner } from "reactstrap";
+
+export const FullPageSpinner = () => (
+  <div className="w-100 h-100 d-flex justify-content-center align-items-center">
+    <Spinner />
+  </div>
+)
