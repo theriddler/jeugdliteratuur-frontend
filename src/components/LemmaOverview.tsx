61d419c8cfc51a1ffbac916bd46d4bb396b66b46
theriddler <riddle.hunter@gmail.com>
2025-11-20 21:23:26 +1300

    can navigate between groups from inside a group

--- src/components/LemmaOverview.tsx
+++ src/components/LemmaOverview.tsx
@@ -14,7 +14,7 @@ export const LemmaOverview = (props: {
   const imageUrl = attributes?.afbeelding?.data?.attributes?.url;
 
   return (
-    <Card className="clickable h-100" onClick={() => navigate(`/teksten/${id}`)}>
+    <Card className="clickable card-hover h-100" onClick={() => navigate(`/teksten/${id}`)}>
       <CardBody>
         <div className="d-flex flex-column h-100">
           <div>
--- src/pages/Level.tsx
+++ src/pages/Level.tsx
@@ -1,34 +1,63 @@
 import { useQuery } from "@apollo/client";
 import { useMemo } from "react";
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
 import { Col, Row } from "reactstrap";
 import { FullPageSpinner } from "../components/FullPageSpinner";
 import { LemmaOverview } from "../components/LemmaOverview";
+import { sortNievauWithKinderFirst } from "../funcs/sortNievauWithKinderFirst";
 import { LEMMATA, LEVELS } from "../queries";
 
 export const Level = () => {
   const { levelId } = useParams();
 
-  const { data: levels, loading: loadingLevel } = useQuery(LEVELS);
-  const { data: lemmata, loading: loadingLemmas } = useQuery(LEMMATA);
+  const { data: levelData, loading: loadingLevel } = useQuery(LEVELS);
+  const { data: lemmataData, loading: loadingLemmas } = useQuery(LEMMATA);
 
-  const level = useMemo(() => levels?.niveaus?.data?.find(l => l.id === levelId), [ levelId, levels?.niveaus?.data ]);
-  const lemmas = useMemo(() => lemmata?.lemmata?.data?.filter(l => l.attributes?.niveau?.data?.id === level?.id), [ lemmata?.lemmata?.data, level?.id ])
+  const sortedLevels = useMemo(() => {
+    const output = [ ...(levelData?.niveaus?.data ?? []) ]
+    output.sort(sortNievauWithKinderFirst)// put kindergarden first
+    return output;
+  }, [ levelData?.niveaus?.data ])
 
+  const levelIndex = useMemo(() => sortedLevels?.findIndex(l => l.id === levelId), [ levelId, sortedLevels ]);
+  const level = useMemo(() => sortedLevels?.[ levelIndex ], [ levelIndex, sortedLevels ]);
+  const lemmas = useMemo(() => lemmataData?.lemmata?.data?.filter(l => l.attributes?.niveau?.data?.id === level?.id), [ lemmataData?.lemmata?.data, level?.id ])
   const loading = useMemo(() => loadingLevel || loadingLemmas, [ loadingLemmas, loadingLevel ])
+
+  console.log(levelIndex, sortedLevels)
+  const previousLevel = useMemo(() => {
+    if (levelIndex === 0) return undefined;
+    return sortedLevels[ levelIndex - 1 ]
+  }, [ levelIndex, sortedLevels ])
+
+  const nextLevel = useMemo(() => {
+    if (levelIndex === sortedLevels.length - 1) return undefined;
+    return sortedLevels[ levelIndex + 1 ]
+  }, [ levelIndex, sortedLevels ])
 
   return (
     <div>
-      {loading && (
-        <div className="w-100 h-100 d-flex align-items-center justify-content-center">
-          <FullPageSpinner />
-        </div>
-      )}
-      <Row>
-        <Col xs={12}>
-          <h4>{level?.attributes?.titel}</h4>
-          <div>
-            {level?.attributes?.beschrijving}
+      <Row className="align-items-center">
+        <Col xs={3}>
+          <div className="text-start">
+            <Link to={`/groep/${previousLevel?.id}`}>
+              {previousLevel?.attributes?.titel}
+            </Link>
+          </div>
+        </Col>
+        <Col xs={6}>
+          <div className="w-100 d-flex justify-content-center">
+            <h4>{level?.attributes?.titel}</h4>
+            <div>
+              {level?.attributes?.beschrijving}
+            </div>
+          </div>
+        </Col>
+        <Col xs={3}>
+          <div className="text-end">
+            <Link to={`/groep/${nextLevel?.id}`}>
+              {nextLevel?.attributes?.titel}
+            </Link>
           </div>
         </Col>
       </Row>
@@ -39,6 +68,11 @@ export const Level = () => {
           </Col>
         ))}
       </Row>
+      {loading && (
+        <div className="w-100 h-100 d-flex align-items-center justify-content-center">
+          <FullPageSpinner />
+        </div>
+      )}
     </div>
   )
 }
