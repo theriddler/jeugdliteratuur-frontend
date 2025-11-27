f494cea6ecbf30f15a193b773881cfe672da47e9
theriddler <riddle.hunter@gmail.com>
2025-11-28 10:07:00 +1300

    tags sort on alfabet

--- src/components/TagTable.tsx
+++ src/components/TagTable.tsx
@@ -1,20 +1,27 @@
-import { useQuery } from "@apollo/client"
-import { TAGS_FOR_SEARCHBAR } from "../queries"
-import { FullPageSpinner } from "./FullPageSpinner";
+import { useQuery } from "@apollo/client";
 import { IconTag } from "@tabler/icons-react";
+import { useMemo } from "react";
 import { useNavigate } from "react-router";
+import { TAGS_FOR_SEARCHBAR } from "../queries";
+import { FullPageSpinner } from "./FullPageSpinner";
 
 export const TagTable = () => {
   const navigate = useNavigate();
 
   const { data: tags, loading } = useQuery(TAGS_FOR_SEARCHBAR);
 
+  const sortedTags = useMemo(() => {
+    const output = [ ...(tags?.tags?.data ?? []) ];
+    output.sort((a, b) => a.attributes?.titel?.localeCompare(b.attributes?.titel ?? '') ?? 0);
+    return output;
+  }, [ tags?.tags?.data ])
+
   if (loading) return <FullPageSpinner />
 
   return (
     <table>
       <tbody>
-        {tags?.tags?.data?.map(t => (
+        {sortedTags?.map(t => (
           <tr onClick={() => navigate(`/tag/${t.id}`)}>
             <td style={{ width: '50px' }}>
               <IconTag />
